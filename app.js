944e628a1b1cb17f2a00b0f9951bbdc658760fa2
Pablo <frankp27@me.com>
2022-01-07 08:02:00 -0500

    finish module part4

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
+ const fs = require('fs');
 
-// const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
 
@@ -148,11 +148,16 @@ if (!portfolioData.projects) {
 
 
 promptUser()
-.then(promptProject)
-.then(portfolioData => {
-  console.log(portfolioData);
-});
+  .then(promptProject)
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
 
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+  });
 
 
 
